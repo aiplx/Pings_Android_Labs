2bc60785c8999994963e82a54f41e169e1f4ecda
Ping Liang <liangping.tse@gmail.com>
2024-02-05 16:30:29 -0500

    Finished Lab step 2 of Lab 4

--- app/src/main/java/algonquin/cst2335/lian0122/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/lian0122/MainActivity.java
@@ -1,15 +1,54 @@
 package algonquin.cst2335.lian0122;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w("MainActivity","In onCreate() - Loading Widgets");
 
+        bt = findViewById(R.id.loginButton);
+        bt.setOnClickListener(clk ->{
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            startActivity( nextPage);
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity","In onStart() - The application is now visible on screen");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity","In onResume() - The application is now responding to user input");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity","In onPause() - The application no longer responds to user input");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity","In onStop() - The application is no longer visible");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity","In onDestroy() - Any memory used by the application is freed");
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/lian0122/SecondActivity.java
@@ -0,0 +1,18 @@
+package algonquin.cst2335.lian0122;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+
+}
